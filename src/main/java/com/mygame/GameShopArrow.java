77002521bcae6f68ca41dec0aab3b1f0c7dcd95f
GameShopLLC <gameshopengine@gmail.com>
2024-12-02 08:11:31 -0500

    GameShopArrow
    A BiNormal Vector Tuple.  Good For All Sorts Of Things

---
+++ src/main/java/com/mygame/GameShopArrow.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mygame;
+
+import com.jme3.math.Vector3f;
+
+/**
+ *
+ * @author lynden
+ * 
+ * This is a BiNormal Vector
+ * 
+ * A Vector Tuple
+ * 
+ * Nicknamed GameShopArrow
+ */
+public class GameShopArrow {
+    
+    public Vector3f a;
+    public Vector3f b;
+    
+    public GameShopArrow(Vector3f a, Vector3f b){
+    
+        this.a = new Vector3f(a);
+        this.b = new Vector3f(b);
+        
+    }
+}
